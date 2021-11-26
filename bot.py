d252fd61d5a50276da468485525242ad7b50effa
Ashwin Shankar <ashwinshankar5@gmail.com>
2021-11-26 01:00:02 -0500

    added load//unload/reload functionality

--- bot.py
+++ bot.py
@@ -113,7 +113,55 @@ async def on_error(event, *args, **kwargs):
             f.write(f"Unhandled message: {args[0]}\n")
         else:
             raise
+# ----------------------------------
+#    Function: load
+#    Description: Command for loading a cog
+#    Inputs:
+#    - ctx: used to access the values passed through the current context
+#    - extension: name of the cog that is to be added
+#    Outputs:
+#    - response from bot
+# ----------------------------------
+@bot.command(name='loadCog',help="loads a specific cog")
+@has_permissions(administrator=True)
+async def load(ctx,extension):
+    bot.load_extension(f"cogs.{extension}")
+    await ctx.send(str(extension)+' cog has been added')
 
+# ----------------------------------
+#    Function: unload
+#    Description: Command for unloading a cog
+#    Inputs:
+#    - ctx: used to access the values passed through the current context
+#    - extension: name of the cog that is to be removed
+#    Outputs:
+#    - response from bot
+# ----------------------------------
+
+@bot.command(name='unloadCog',help="unloads a specific cog")
+@has_permissions(administrator=True)
+async def unload(ctx,extension):
+    bot.unload_extension(f"cogs.{extension}")
+    await ctx.send(str(extension)+' cog has been removed')
+
+
+# ----------------------------------
+#    Function: reload
+#    Description: Command for reloading a cog after an update is made
+#    Inputs:
+#    - ctx: used to access the values passed through the current context
+#    - extension: name of the cog that is to be reloaded
+#    Outputs:
+#    - response from bot
+# ----------------------------------
+
+@bot.command(name='reloadCog',help="reloads a specific cog")
+@has_permissions(administrator=True)
+async def reload(ctx,extension):
+    bot.unload_extension(f"cogs.{extension}")
+    bot.load_extension(f"cogs.{extension}")
+
+    await ctx.send(str(extension)+' cog has been reloaded')
 
 # ----------------------------------
 #    Function: on_member_join(member)
